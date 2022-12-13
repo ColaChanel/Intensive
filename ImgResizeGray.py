8753a161d987f6ee474b2b66f2eac17a3800195d
ColaChanel <47081014+ColaChanel@users.noreply.github.com>
2022-12-13 17:46:31 +0300

    Ready code

--- ImgResizeGray.py
+++ ImgResizeGray.py
@@ -1,14 +1,27 @@
+from tempfile import TemporaryDirectory
 from PIL import Image
 import os, sys
+from matplotlib import image
+from matplotlib.pyplot import show
+import numpy as np
 
-path = "Drinks/Water/Good"
-dirs = os.listdir(path)
-newFolder = "New Drinks/Water/Good"
 
-for item in dirs:
-    #i = 1
-    img = Image.open(os.path.join(path, item))
-    img = img.resize((300, 300))
-    img = img.convert("L")
-    img.save(os.path.join(newFolder, item))
-    #i+=1
+sMainPath = "C:\\Users\\Интенсив\\Desktop\\data"
+sNewPath = "C:\\Users\\Интенсив\\Desktop\\NewData"
+nameDrags = np.array(os.listdir(sMainPath))
+print(nameDrags)
+
+for i in nameDrags:
+    FolderName = sMainPath + "\\" + str(i)
+    nameInDrags = np.array(os.listdir(FolderName))
+    for j in nameInDrags:
+        path = FolderName + "\\" + j
+        FolderBadOrGood = np.array(os.listdir(path))
+        for BadOrGood in FolderBadOrGood:
+            FilesInDrags = path + "\\" + BadOrGood
+            files = np.array(os.listdir(FilesInDrags))
+            for z in files:
+                img = Image.open(FilesInDrags + "\\" + z)
+                img = img.resize((300, 300))
+                img = img.convert("L")
+                img.save(sNewPath + "\\" + i + "\\"  + j + "\\"  + BadOrGood + "\\"  + z)
