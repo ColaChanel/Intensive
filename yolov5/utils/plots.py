a34b28072c049ef25c674840cc2f107144b19b1e
X_O_R_S <60074069+danbka220@users.noreply.github.com>
2022-12-20 12:00:55 +0300

    plot fix

--- yolov5/utils/plots.py
+++ yolov5/utils/plots.py
@@ -54,11 +54,11 @@ def check_pil_font(font=FONT, size=10):
     font = Path(font)
     font = font if font.exists() else (CONFIG_DIR / font.name)
     try:
-        return ImageFont.truetype(str(font) if font.exists() else font.name, size, encoding='UTF-8')
+        return ImageFont.truetype(str(font) if font.exists() else font.name, size)
     except Exception:  # download if missing
         try:
             check_font(font)
-            return ImageFont.truetype(str(font), size, encoding='UTF-')
+            return ImageFont.truetype(str(font), size)
         except TypeError:
             check_requirements('Pillow>=8.4.0')  # known issue https://github.com/ultralytics/yolov5/issues/5374
         except URLError:  # not online
