c2e2f313b9d46ffa1a918f6142250ec59787cb73
X_O_R_S <60074069+danbka220@users.noreply.github.com>
2022-12-21 16:31:00 +0300

    deleted auto open browser

--- run.py
+++ run.py
@@ -116,10 +116,5 @@ def update_output(content):
         ]
         return content, img, table
 
-def open_browser():
-    if not os.environ.get("WERKZEUG_RUN_MAIN"):
-        webbrowser.open('http://127.0.0.1:5000/')
-
 if __name__ == '__main__':
-    Timer(1, open_browser).start()
     app.run_server(debug=True, port=5000)
